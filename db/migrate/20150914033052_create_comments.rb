acb7e29edaf989dda88a36b606d0b2c343ffed2c
David Stulajter <davidstulajter@gmail.com>
2015-09-14 17:38:15 -0500

    Checkpoint 28 completed

---
+++ db/migrate/20150914033052_create_comments.rb
@@ -0,0 +1,15 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :post, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :comments, :posts
+  end
+end
+class Post < ActiveRecord::Base
+  has_many :coomments
+end
+  
