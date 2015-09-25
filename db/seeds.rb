22e351eef90d95dba684bac3d3f72b2df76fdace
David Stulajter <davidstulajter@gmail.com>
2015-09-25 15:31:22 -0500

    assignment 33, seeding data, completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,21 @@ posts = Post.all
    body: RandomData.random_paragraph
   )
 end
+if Post.where(:id=>0).blank?
+  Post.create(
+    id: 0,
+    title: "UNIQUE TITLE",
+    body: "UNIQUE BODY"
+    )
+  puts "Created Unique Post Here"
+end
+if Comment.where(:id=>0).blank?
+  Comment.create(
+    id: 0,
+    body: "UNIQUE BODY"
+    )
+  puts "Created Unique Comment Here"
+end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
