ace70bd246efd501302ef78539fdfd0138079d50
David Stulajter <davidstulajter@gmail.com>
2015-10-17 15:10:25 -0500

    Checkpoint-34-topics and posts finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915035113) do
+ActiveRecord::Schema.define(version: 20151016185126) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -34,7 +34,10 @@ ActiveRecord::Schema.define(version: 20150915035113) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +45,14 @@ ActiveRecord::Schema.define(version: 20150915035113) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,27 @@
 include RandomData
 
+#Create Topics
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all 
+
+#Create Posts
 50.times do
   Post.create!(
+   topic: topics.sample, 
    title: RandomData.random_sentence,
    body: RandomData.random_paragraph
    )
 end
+
+
 posts = Post.all
+
+#Create Comments
 100.times do 
   Comment.create!(
    post: posts.sample,
@@ -14,6 +29,7 @@ posts = Post.all
   )
 end
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 
