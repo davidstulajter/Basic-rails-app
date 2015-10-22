aa9b23d7f34cfc20258f457ae435fa77522246fc
David Stulajter <davidstulajter@gmail.com>
2015-10-22 17:06:33 -0500

    checkpoint 40 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
   
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -14,5 +15,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 100 },
             format: { with: EMAIL_REGEX }
-  has_secure_password          
+  has_secure_password
+
+  enum role: [:member, :admin]          
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020182949) do
+ActiveRecord::Schema.define(version: 20151022173250) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20151020182949) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
